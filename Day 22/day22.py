a987daaadc866000c8fc2d8a55e078650d7e5083
Pratyush <balajipratyush@gmail.com>
2024-12-22 21:03:23 -0500

    day 22 solution

--- Day 22/day22.py
+++ Day 22/day22.py
@@ -22,13 +22,13 @@ def nextSecretNumberBitwise(secret):
     nextNumber = secret
     
     nextNumber ^= (secret << 6) # secret * 64
-    nextNumber %= 16777216
+    nextNumber &= 16777215
     
     nextNumber ^= (nextNumber >> 5) # secret // 32
-    nextNumber %= 16777216
+    nextNumber &= 16777215
     
     nextNumber ^= (nextNumber << 11) # secret * 2048
-    nextNumber %= 16777216 # secret % (2**24)
+    nextNumber &= 16777215 # secret % (2**24)
     
     return nextNumber
 
@@ -41,4 +41,37 @@ def partOne():
         total+=secret
     return total
 
-print(partOne())
+def generatePrices(initial, steps=2000):
+    secret = initial
+    prices = []
+    for _ in range(steps + 1):  # +1 to calculate changes
+        prices.append(secret % 10)  # Extract the last digit
+        secret = nextSecretNumberBitwise(secret)
+    return prices
+
+def generateChanges(prices):
+    changes = []
+    for i in range(len(prices)-1):
+        changes.append(prices[i+1] - prices[i])
+    return changes
+
+def partTwo():
+    from collections import defaultdict
+    priceChangeMemo = defaultdict(int)
+
+    for i in data:
+        buyer = int(i.strip())
+        prices = generatePrices(buyer)
+        changes = generateChanges(prices)
+        found = set()
+        for j in range(len(changes)-3):
+            index = (changes[j],changes[j+1],changes[j+2],changes[j+3])
+            if index in found:
+                continue
+            priceChangeMemo[index] += prices[j+4]
+            found.add(index)
+    
+    return max(priceChangeMemo.values())
+
+# print(partOne())
+# print(partTwo())
